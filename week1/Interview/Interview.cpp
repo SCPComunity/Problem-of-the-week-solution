555c53c4a815f4dd5a81caedc8b4facae0bbf333
FatimaALzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2023-04-01 04:27:23 +0300

    Power: 1300

--- week1/Interview/Interview.cpp
+++ week1/Interview/Interview.cpp
@@ -32,7 +32,7 @@ int32_t main() {
     //ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     int t;
-    cin >> t;
+    cin >> t; 
     while (t--) {
         cin >> n;
         a.resize(n);
